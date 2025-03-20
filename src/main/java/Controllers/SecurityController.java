67d7f92d9a9c4c83d3ccf61c022a5e0db0be2d73
Elendoin <elendoin23@gmail.com>
2025-03-20 03:01:14 +0100

    feat(api): Implemented /login and /register pages

--- src/main/java/Controllers/SecurityController.java
+++ src/main/java/Controllers/SecurityController.java
@@ -2,6 +2,8 @@ package Controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import Models.User;
 
@@ -18,16 +20,47 @@ public class SecurityController {
     }
 
     static class LoginRequest {
+        public String email;
+        public String password;
+    }
+
+    static class RegitserRequest {
         public String username;
+        public String surname;
+        public String email;
         public String password;
     }
 
     @GetMapping("/api/users")
     public ResponseEntity<?> getUsers(){
         if(users.isEmpty()){
-            return ResponseEntity.noContent().build();
+            return ResponseEntity.status(404).build();
         }
         return ResponseEntity.ok(users);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest request){
+        for(User user : users){
+            if(request.email.equals(user.getEmail()) && request.password.equals(user.getPassword())){
+                return ResponseEntity.ok("Logged in successfully" + user.getId());
+            }
+        }
+        return ResponseEntity.status(401).build();
+
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody RegitserRequest request) {
+        for(User user : users){
+            if(request.email.equals(user.getEmail())){
+                return ResponseEntity.badRequest().body("Email already in use!");
+            }
+        }
+
+        User user = new User(users.size()+1L, request.username, request.surname, request.email, request.password);
+        users.add(user);
+        return ResponseEntity.ok("User successfully registered!" + user.getId());
     }
 
 
